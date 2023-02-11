16755c679444f9d532950fbce0128a17df7ea7fa
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-11 16:20:31 +0500

    Task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Type the number ");
+int number = int.Parse(Console.ReadLine());
+
+int count = number * 2 + 1;
+int ind = 0;
+int writeNum = -number - 1;
+
+while(ind < count){
+    Console.WriteLine(writeNum = writeNum + 1);
+    ind++;
+} 
