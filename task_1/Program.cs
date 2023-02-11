ca01b5f8e50f4e1d128b18ebe8fc5dd11c66a9a0
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-11 15:21:33 +0500

    First Task.

---
+++ task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,100);
+int result = numberA * numberA;
+Console.Write("Квадрат числа ");
+Console.Write(numberA);
+Console.Write(" равен: ");
+Console.WriteLine(result);
