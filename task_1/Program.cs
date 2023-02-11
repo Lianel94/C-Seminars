db6d7dcfeb11e479461a9e9cbf5739c454cbf3cc
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-11 15:27:37 +0500

    Добавили второе решение для task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,6 +1,14 @@
-﻿int numberA = new Random().Next(1,100);
-int result = numberA * numberA;
+﻿// int numberA = new Random().Next(1,100);
+// int result = numberA * numberA;
+// Console.Write("Квадрат числа ");
+// Console.Write(numberA);
+// Console.Write(" равен: ");
+// Console.WriteLine(result);
+
+Console.Write("Type the number");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
 Console.Write("Квадрат числа ");
-Console.Write(numberA);
+Console.Write(number);
 Console.Write(" равен: ");
-Console.WriteLine(result);
+Console.Write(result);
