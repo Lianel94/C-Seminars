0a75cfd07c00a07a3231e3d869315e4fbd0cecb5
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-11 15:49:26 +0500

    Task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Type the first number ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Type the second number ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 < number2) 
+{
+    Console.WriteLine("maximum number is " + number2 + " and minimum number is " + number1);
+} 
+else 
+{
+    Console.WriteLine("maximum number is " + number1 + " and minimum number is " + number2);
+}
