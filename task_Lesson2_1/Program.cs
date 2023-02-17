4cd7f92d60efe5ec61d98a65fefd446d2bf2d7c0
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-17 16:34:39 +0500

    added the second lesson task_lesson_2_1 and solved the first task

---
+++ task_Lesson2_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Write a program, choosing a random number from [10, 99] adn showing the biggest digit of it 78 - max 8
+Console.Clear(); // clear the console
+
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+int max = a1;
+if(a2 > max) max = a2;
+Console.WriteLine($"Maximum digit of the number {number} is {max}");
