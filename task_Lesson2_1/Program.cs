d0dd68375f03eea9b29d2501dd4e56528701e22d
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-17 17:33:17 +0500

    Solved one more task from lesson 2

--- task_Lesson2_1/Program.cs
+++ task_Lesson2_1/Program.cs
@@ -34,3 +34,17 @@ else
 {
 	Console.WriteLine($"{userNum2} не кратно {userNum1} и остаток = {userNum1 % userNum2}");
 }
+
+// число и проверяет кратно ли оно 7 и 23 одновременно if(a>b && a<c) && - и; || - или if((a>b || a<c) && (x>b || x<c) || d==5 )
+// 14 - no, 46 - no, 161 - yes
+
+Console.Write("Type the number ");
+int userNumber = int.Parse(Console.ReadLine());
+if (userNumber % 7 == 0 && userNumber % 23 == 0) 
+{
+	Console.WriteLine($"The number {userNumber} кратно числам 7 и 23 одновременно");
+}
+else
+{
+	Console.WriteLine($"The number {userNumber} не кратно 7 и 23 одновременно");
+}
