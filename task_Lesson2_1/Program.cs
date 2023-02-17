c2e11df203d4ad84e555552605b0864f96314894
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-17 16:52:35 +0500

    added the second lesson task_lesson_2_1 and solved the second task

--- task_Lesson2_1/Program.cs
+++ task_Lesson2_1/Program.cs
@@ -7,3 +7,16 @@ int a2 = number % 10;
 int max = a1;
 if(a2 > max) max = a2;
 Console.WriteLine($"Maximum digit of the number {number} is {max}");
+
+
+// write a program with a random 3-digir number (359) and delete the second digit from it (39)
+
+Console.Write("Type the 3-digit number. Example: 247 ");
+int userNum = int.Parse(Console.ReadLine());
+int digit1 = userNum / 100; //2
+int digit2 = userNum % 10; //7
+
+Console.WriteLine($"Two-digit number of the number {userNum} without the middle digit is {digit1 * 10 + digit2}");
+Console.WriteLine($"Two-digit number of the number {userNum} without the middle digit is {digit1}{digit2}");
+
+
