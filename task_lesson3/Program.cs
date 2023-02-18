3e89cb77b857f7c13bf1261a748a86a13f0e6d65
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-18 17:35:03 +0500

    Lesson_3 задача 3

--- task_lesson3/Program.cs
+++ task_lesson3/Program.cs
@@ -9,23 +9,23 @@ int y = int.Parse(Console.ReadLine());
 
 if (x > 0 && y > 0)
 {
-    Console.WriteLine("1");
+	Console.WriteLine("1");
 }
 else if (x < 0 && y > 0)
 {
-    Console.WriteLine("2");
+	Console.WriteLine("2");
 }
 else if (x < 0 && y < 0)
 {
-    Console.WriteLine("3");
+	Console.WriteLine("3");
 }
 else if (x > 0 && y < 0)
 {
-    Console.WriteLine("4");
+	Console.WriteLine("4");
 }
 else if (x == 0 || y == 0)
 {
-    Console.WriteLine("На оси");
+	Console.WriteLine("На оси");
 }
 
 // по заданной четверти, диапозон возможных координат x и y (x>0;y<0)
@@ -48,29 +48,47 @@ int quarter = int.Parse(Console.ReadLine());
 
 switch (quarter)
 {
-    case 1:
-        {
-            Console.WriteLine("x > 0 and y > 0");
-            break;
-        }
-    case 2:
-        {
-            Console.WriteLine("x < 0 and y > 0");
-            break;
-        }
-    case 3:
-        {
-            Console.WriteLine("x < 0 and y < 0");
-            break;
-        }
-    case 4:
-        {
-            Console.WriteLine("x > 0 and y < 0");
-            break;
-        }
-    default:
-        {
-            Console.WriteLine("Введена неправильная четверть");
-            break;
-        }
+	case 1:
+		{
+			Console.WriteLine("x > 0 and y > 0");
+			break;
+		}
+	case 2:
+		{
+			Console.WriteLine("x < 0 and y > 0");
+			break;
+		}
+	case 3:
+		{
+			Console.WriteLine("x < 0 and y < 0");
+			break;
+		}
+	case 4:
+		{
+			Console.WriteLine("x > 0 and y < 0");
+			break;
+		}
+	default:
+		{
+			Console.WriteLine("Введена неправильная четверть");
+			break;
+		}
 }
+
+// на вход - координаты двух точек и находит расстояние между ними в два д пространстве.
+// a (3, 6), b (2 1)
+// sqrt((3-2)^2 + (6-1)^2)
+// double s = Math.Sqrt(...);
+
+Console.Write("Введите координаты 1 числа по x: ");
+int x1 = int.Parse(Console.ReadLine()); // double xi = double.Parse(Console.ReadLine());
+Console.Write("Введите координаты 1 числа по y: ");
+int y1 = int.Parse(Console.ReadLine()); // double xi = double.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты 2 числа по x: ");
+int x2 = int.Parse(Console.ReadLine()); // double xi = double.Parse(Console.ReadLine());
+Console.Write("Введите координаты 2 числа по y: ");
+int y2 = int.Parse(Console.ReadLine()); // double xi = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Расстояние между двумя точками с координатами {x1}-{y1} и {x2}-{y2} равно {Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 2)}");
+// если через double - math.round - no need
