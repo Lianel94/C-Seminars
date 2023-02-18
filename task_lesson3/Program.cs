10f3035d21ed42c95d3a1567eb1851d59ea6815c
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-18 18:04:29 +0500

    Lesson_3 задача 4.2

--- task_lesson3/Program.cs
+++ task_lesson3/Program.cs
@@ -115,3 +115,28 @@ for (int i = 0; i < userNum; i++)
 {
 	Console.Write($"{result[i]} ");
 }
+
+Console.Clear();
+Console.Write("Write the number N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N;
+
+if (N < 0)
+{
+	i = N;
+	m = -N;
+	N = -1;
+}
+
+int[] Result = new int[m];
+int j = 0;
+for(; i <= N; i++)
+{
+	Result[j] = i * i;
+	j++;
+}
+for (j = 0; j < m; j++)
+{
+	Console.Write($"{Result[j]} ");
+}
