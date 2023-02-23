07479cebfc86913b4c83c5d3f084578bbdd578b7
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-23 15:46:44 +0500

    third lesson 3 task

--- task_lesson4/Program.cs
+++ task_lesson4/Program.cs
@@ -3,12 +3,12 @@
 
 int GetSum(int n)
 {
-    int sum = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        sum += i;
-    }
-    return sum;
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+	return sum;
 }
 
 Console.Clear();
@@ -20,23 +20,38 @@ Console.WriteLine($"The sum from 1 to {n} is {GetSum(n)}");
 
 int GetDigitNumber(int userNum)
 {
-    int i = 0;
+	int i = 0;
 
-    if (userNum < 0)
-    {
-        userNum *= (-1);
-    }
+	if (userNum < 0)
+	{
+		userNum *= (-1);
+	}
 
-    while (userNum > 0)
-    {
-        userNum /= 10;
-        i++;
-    }
-    return i;
+	while (userNum > 0)
+	{
+		userNum /= 10;
+		i++;
+	}
+	return i;
 }
 
 Console.Write("Type your number: ");
 int userNum = int.Parse(Console.ReadLine());
 Console.WriteLine($"The number of digits in the {userNum} is {GetDigitNumber(userNum)}");
 
+// на вход число и выдает произведение от 1 до числа
+//5 - 1*2*3*4*5
 
+int Multiply(int num)
+{
+	int result = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+Console.Write("Type your number: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"The sum from 1 to {n} is {Multiply(num)}");
