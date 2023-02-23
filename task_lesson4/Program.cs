581a7b7529b23c2bfeea227523c498160ba57292
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-23 16:40:06 +0500

    third lesson 4 task

--- task_lesson4/Program.cs
+++ task_lesson4/Program.cs
@@ -62,3 +62,28 @@ Console.Write("Type your number: ");
 int num = int.Parse(Console.ReadLine());
 Console.WriteLine($"The multiplication from 1 to {n} is {Multiply(num)}");
 
+
+//вывести массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+// [1,0,1,1,0,1,0,0]
+// int a = new Random().Next(2,7) / 23456
+// int a = new Random().Next(2) / 0 1
+
+int[] GetBinaryArray(int size)
+{
+	int[] result = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		result[i] = new Random().Next(2);
+	}
+	return result;
+}
+
+int[] array = GetBinaryArray(8);
+
+Console.WriteLine($"[ {String.Join(", ", array)} ]"); // краткий вариант вывода
+
+for (int i = 0; i < array.Length - 1; i++)
+{
+	Console.Write($"{array[i]}, "); // вывод до последнего элемента
+}
+Console.WriteLine($"{array[array.Length - 1]}"); // вывод последнего элемента
