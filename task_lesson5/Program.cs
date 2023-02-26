84aba0f92ecd539c4e64c3339c955b4aa1244570
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-26 20:47:32 +0500

    some corrections

--- task_lesson5/Program.cs
+++ task_lesson5/Program.cs
@@ -34,16 +34,17 @@ Console.Clear();
 
 //2nd OPTION
 
-//int [] GetRandomArray(int size, int minValue, int maxValue)
+// int[] GetRandomArray(int size, int minValue, int maxValue)
 // {
-// 	int[] result = new int[size];
-// 	for(int i = 0; i < sizeof; i++)
+// 	int [] result = new int[size];
+// 	for(int i = 0; i < size; i++)
 // 	{
 // 		result[i] = new Random().Next(minValue, maxValue + 1);
 // 	}
+// 	return result;
 // }
 
-// int[] array = GetRandomArray(12, -9, 9);
+// int[] array = GetRandomArray(4, -9, 9);
 // Console.WriteLine(String.Join(", ", array));
 
 int[] GetArray(int size)
