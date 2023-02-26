23525a1e8249c811a6f041335720142733f4e93c
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-26 20:39:40 +0500

    5th lesson 2nd exercise

---
+++ task_lesson5_2ex/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАМЕНИТЬ + ЭЛЕМЕНТЫ МАССИВА НА -, И НАОБОРОТ
+
+int [] GetRandomArray(int size, int minValue, int maxValue)
+{
+	int[] result = new int[size];
+	for(int i = 0; i < sizeof; i++)
+	{
+		result[i] = new Random().Next(minValue, maxValue + 1);
+	}
+}
+
+int[] array = GetRandomArray(4, -9, 9);
+Console.WriteLine(String.Join(", ", array));
