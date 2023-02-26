8e9eee56da5201f9babdbebfd307a80a81778e82
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-26 20:22:56 +0500

    5th Lesson 2nd option FOREACH comments second option universal form to get a mass

--- task_lesson5/Program.cs
+++ task_lesson5/Program.cs
@@ -34,6 +34,18 @@ Console.Clear();
 
 //2nd OPTION
 
+//int [] GetRandomArray(int size, int minValue, int maxValue)
+// {
+// 	int[] result = new int[size];
+// 	for(int i = 0; i < sizeof; i++)
+// 	{
+// 		result[i] = new Random().Next(minValue, maxValue + 1);
+// 	}
+// }
+
+// int[] array = GetRandomArray(12, -9, 9);
+// Console.WriteLine(String.Join(", ", array));
+
 int[] GetArray(int size)
 {
 	int[] result = new int[size];
