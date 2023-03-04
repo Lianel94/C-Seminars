323b676ababc8f9a4f37ba7c2fa9b115439569e3
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-03-04 17:25:22 +0500

    Lesson 6 Reverse Array

---
+++ task_lesson6/Program.cs
@@ -0,0 +1,31 @@
+﻿// перевернуть массив: 12345 - 54321
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+	int[] result = new int[size];
+	for(int i = 0; i < size; i++)
+	{
+		result[i] = new Random().Next(minValue, maxValue + 1);
+	}
+	return result;
+}
+
+int[] SwitchElements(int[] array)
+{
+	int[] switchResult = new int[array.Length];
+	for(int i = 1; i < array.Length; i++)
+	{
+		switchResult[0] = array[array.Length - 1];
+		switchResult[i] = array[array.Length - i - 1];
+	}
+	return switchResult;
+}
+
+Console.Write("Type the size of the array: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = GetArray(size, 1, 99);
+Console.WriteLine(String.Join(", ", array));
+
+Console.WriteLine($"Switched elements: {String.Join(", ", SwitchElements(array))}");
+
