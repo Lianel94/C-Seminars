8391b131a2e556f5b2977efb007a4dc445eb6c30
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-03-11 20:00:13 +0500

    нахождение суммы цифр в числе

--- task_lesson9_ex3/Program.cs
+++ task_lesson9_ex3/Program.cs
@@ -1,1 +1,23 @@
-﻿// 
+﻿// написать прогу, на вход число и возвращать сумму его цифр
+// 453 - 12
+
+Console.Clear();
+
+Console.Write("Type the number: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The sum of the gigits in the number {userNumber} is {GetSum(userNumber)}");
+
+// int sum = 0;
+// while(userNumber > 0)
+// {
+// 	sum += userNumber % 10;
+// 	userNumber /= 10;
+// }
+// Console.WriteLine($"{sum}");
+
+int GetSum(int userNumber)
+{
+	if (userNumber == 0) return 0;
+	return (userNumber % 10 + GetSum(userNumber / 10));
+}
