16bd7351f734a197bb4e4ce83e26e0433b300022
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-03-11 20:18:58 +0500

    возведение в степень через рекурсию

---
+++ task_lesson9_ex4/Program.cs
@@ -0,0 +1,17 @@
+﻿// прога, на вход вдва числа и возврат первого в целую стпень второго
+// a = 3, b = 5 - 243
+
+Console.Clear();
+
+Console.Write("Type the number: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Type the pow: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The result of number {m} in the power of {n} is {GetPower(m, n)}");
+
+int GetPower(int number, int rank)
+{
+	if(rank == 0) return 1;
+	return (number * GetPower(number, rank - 1));
+}
