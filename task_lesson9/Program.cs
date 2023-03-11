73c70a7ef1c43c2f193bb0d3d039ceb0bf39ade4
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-03-11 19:30:33 +0500

    recursion

---
+++ task_lesson9/Program.cs
@@ -0,0 +1,23 @@
+﻿// 
+
+Console.Clear();
+Console.Write("Type N: ");
+int n = int.Parse(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+{
+	Console.WriteLine($"{i}");
+}
+Console.WriteLine();
+Console.WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int n)
+{
+	if(n == 1)
+	{
+		Console.WriteLine(1);
+		return "1";
+	}
+	string s = PrintNumbers(n - 1) + " " + n.ToString();
+	Console.WriteLine(s);
+	return s;
+}
